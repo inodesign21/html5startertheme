062660e7c41955e43e162bd6b98d21633c1e2511
inodesign21 <inodesign21@gmail.com>
2014-10-25 19:35:23 -0200

    rest api test

---
+++ js/main.js
@@ -0,0 +1,102 @@
+
+ window.onload = function () {
+
+   // Ajustes do Multiseleção classe multiselect
+    $('.multiselect').multiselect({
+        numberDisplayed: 2,
+        buttonWidth: '172px',
+        maxHeight: 150,
+        nonSelectedText: 'Selecione',
+        enableFiltering: true,
+        filterBehavior: 'both',
+        enableCaseInsensitiveFiltering: false,
+        filterPlaceholder: 'Procure',
+        includeSelectAllOption: true,
+        selectAllText: 'Todos'
+    });
+
+  // Ajustes seleção bootstrap classe selectpicker
+    $('.selectpicker').selectpicker({
+      size: 4,
+      width: '172px'
+     });
+
+// Instance the tour
+    var tour = new Tour({
+      steps: [
+      {
+        element: ".one",
+        title: "Welcome",
+        content: "Welcome to our app, take this tour to be familirized with it."
+      },
+          {
+        element: ".two",
+        title: "This Image",
+        content: "In this application we generate random placeholder images for any case."
+      },
+      {
+        element: ".three",
+        title: "This Image",
+        content: "In this application we generate random placeholder images for any case."
+      },
+      {
+        element: ".four",
+        title: "This Image",
+        content: "In this application we generate random placeholder images for any case."
+      },
+      {
+        element: ".five",
+        title: "This Image",
+        content: "In this application we generate random placeholder images for any case."
+      }  
+    ],
+      backdrop: true,
+      storage: false
+    });
+
+    tour.init();
+
+
+    // Botão inicia tour (todo: somente para mobile)
+    $('#tour').click(function(e){
+    tour.start();
+    e.preventDefault(); 
+    });
+
+    
+    // Inicia slider range With JQuery
+    $("#ex2").slider({
+        tooltip: 'always'
+    });
+
+    // Gallery
+    document.getElementById('links').onclick = function (event) {
+    event = event || window.event;
+    var target = event.target || event.srcElement,
+        link = target.src ? target.parentNode : target,
+        options = {index: link, event: event},
+        links = this.getElementsByTagName('a');
+    blueimp.Gallery(links, options);
+};
+
+};
+
+
+
+// JQuery rest api test
+(function() {
+          var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+          $.getJSON( flickerAPI, {
+            tags: "mount rainier",
+            tagmode: "any",
+            format: "json"
+          })
+            .done(function( data ) {
+              $.each( data.items, function( i, item ) {
+                $( "<img>" ).attr( "src", item.media.m ).appendTo( "#images" );
+                if ( i === 3 ) {
+                  return false;
+                }
+              });
+            });
+        })();
